954bdf12bcd20d9c2f30e5bd2e2ca27377e09385
Maxim Berdyshev <36857730+maximberdyshev@users.noreply.github.com>
2025-10-08 20:42:56 +0500

    lesson2 (#2)

--- internal/telegram/client.go
+++ internal/telegram/client.go
@@ -4,11 +4,37 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"strconv"
+)
+
+const (
+	tgEndpointAPI = "https://api.telegram.org/bot"
 )
 
 type Client struct {
 	token string
+}
+
+type Update struct {
+	UpdateID int `json:"update_id"`
+	Message  *struct {
+		MessageID int    `json:"message_id"`
+		From      *User  `json:"from"`
+		Chat      Chat   `json:"chat"`
+		Text      string `json:"text"`
+	} `json:"message"`
+}
+
+type User struct {
+	ID        int    `json:"id"`
+	Username  string `json:"username"`
+	FirstName string `json:"first_name"`
+}
+
+type Chat struct {
+	ID int64 `json:"ID"`
 }
 
 type sendMessageRequest struct {
@@ -20,8 +46,39 @@ func NewClient(token string) *Client {
 	return &Client{token: token}
 }
 
+func (c *Client) GetUpdates(offset int) ([]Update, error) {
+	url := fmt.Sprintf("%s%s/getUpdates", tgEndpointAPI, c.token)
+
+	reqURL := url + "?offset=" + strconv.Itoa(offset)
+	resp, err := http.Get(reqURL)
+	if err != nil {
+		return nil, fmt.Errorf("http get: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("read reasponse body: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("telegram API returned status: %d, desc: %s", resp.StatusCode, string(body))
+	}
+
+	var result struct {
+		OK     bool     `json:"ok"`
+		Result []Update `json:"result"`
+	}
+
+	if err := json.Unmarshal(body, &result); err != nil {
+		return nil, fmt.Errorf("unmarshal response :%w", err)
+	}
+
+	return result.Result, nil
+}
+
 func (c *Client) SendMessage(chatID, text string) error {
-	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", c.token)
+	url := fmt.Sprintf("%s%s/sendMessage", tgEndpointAPI, c.token)
 
 	payload := sendMessageRequest{
 		ChatID: chatID,
@@ -40,7 +97,7 @@ func (c *Client) SendMessage(chatID, text string) error {
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("telegram API returned status %d", resp.StatusCode)
+		return fmt.Errorf("telegram API returned status: %d", resp.StatusCode)
 	}
 
 	return nil
